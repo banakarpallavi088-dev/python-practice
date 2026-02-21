5303c68c0d70b2a6b313d8d4344ff8803bbcd4c6
Pallavi <banakarpallavi088@gmail.com>
2026-02-21 12:43:34 +0530

    modified file

--- day3/class/encapsulation.py
+++ day3/class/encapsulation.py
@@ -5,7 +5,19 @@ class fac:
         print("i am from karnataka",self.__a)
 class A(fac):
     def show1(self):
-       pass   # print(super()._a)
+       pass   # print(super().__a) we can't access it
 ob=A()
 ob.show()
-ob.show1()
+ob.show1() # abstraction not show the internal working but encapsulation wrapping the attributes amd methods
+# access the private attributes
+class B:
+    def __init__(self,location):
+        self.__location=location
+    def set_loc(self,location):
+        self.__location=location
+    def get_loc(self):
+        return self.__location
+
+o=B("karnataka")
+print(o.get_loc())
+print(o.__location)
